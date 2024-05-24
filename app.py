e4f6327fda503da27faca6291e089722a4bdab5f
XB <xiabai227661@gmail.com>
2024-05-24 15:54:08 +0800

    init

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, send_from_directory
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import time
+import threading
+import temp
+app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1
+
+def generate_image():
+    while True:
+        temp.get()
+        time.sleep(1.5)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/image')
+def get_image():
+    return send_from_directory('static/images', 'plot.png')
+
+if __name__ == '__main__':
+    thread = threading.Thread(target=generate_image)
+    thread.start()
+    app.run(debug=True)
---
+++ temp.py
@@ -0,0 +1,35 @@
+# import requests
+import time
+import qrcode
+from PIL import Image
+
+courseSchedId = ''
+
+def generate_qr_code(url):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(url)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+    return img
+
+
+def get():
+    current_timestamp_seconds = time.time()
+    current_timestamp_milliseconds = int(current_timestamp_seconds * 1000)
+    # print(current_timestamp_milliseconds)
+
+    str = f'http://iclass.buaa.edu.cn:8081/app/course/stu_scan_sign.action?courseSchedId={courseSchedId}&timestamp={current_timestamp_milliseconds}'
+
+    qr_code_img = generate_qr_code(str)
+    # qr_code_img.show()
+    qr_code_img.save('static/images/plot.png')
+
+
+# response = requests.get(url=str, cookies=cookie)
+# print(response.text)
